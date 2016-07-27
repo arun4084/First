c89786af8bfe33335566ae5f326ecd4f6ac6dac1
arun4084 <arunsharma4084@gmail.com>
2016-07-27 08:21:35 +0530

    example
    example of java

---
+++ Example2/src/Example2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ArunSharma
+ */
+public class Example2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num;
+        num = 10;
+        System.out.println("This is num : " +num);
+        
+        num = num*2;
+        System.out.print("The value of num*2 is : ");
+        System.out.println(num);
+                
+    }
+    
+}
